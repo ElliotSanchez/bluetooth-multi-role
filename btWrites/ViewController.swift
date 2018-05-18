d1fbb3440dde026572142e382edb2ea46f93e06b
Elliot Sanchez <username@usernames-macbook-pro.local>
2018-05-18 11:40:13 -0500

    Initial Commit

---
+++ btWrites/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  btWrites
+//
+//  Created by Username on 5/18/18.
+//  Copyright © 2018 ESanchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
